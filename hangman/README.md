21cf63c3447a61fe02864dbac64b0b2318c9098f
Naoki Endo <naoki.endo0529@gmail.com>
2019-11-29 16:23:07 +0900

    hey

---
+++ hangman/README.md
@@ -0,0 +1,2 @@
+# hangman
+just test.
---
+++ hangman/seiko.py
@@ -0,0 +1,10 @@
+Python 3.7.4 (default, Oct 12 2019, 19:06:48) 
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable. Visit
+http://www.python.org/download/mac/tcltk/ for current information.
+>>> print("Hello world!")
+Hello world!
+>>> print("hello")
+hello
+>>> 
